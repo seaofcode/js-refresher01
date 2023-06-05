8d32768ffcc1aa70fdd083e70fd34c1e6a295c7d
web-mjp <seaofcode@gmail.com>
2023-06-05 09:42:15 -0500

    Update app.js

---
+++ app.js
@@ -0,0 +1,32 @@
+// Common Helper Methods
+// ---------------------------
+// document.write('hello world');
+// alert('hellow world');
+// console.log('hello world');
+
+// Statements and Comments
+// console.log('statement');
+
+// Variables - Store, Access, Modify === Value
+// Declare, Assignment Operator, Assign Value
+// let FirstName = "Matthew";
+// assign value later, modify existing
+// let address, zip, state;
+// address = "101 Main Street"
+
+// Numbers are loosely typed
+// let number = 40;
+// number + 5;
+// console.log(number);
+
+// const slices = 10;
+// const children = 3;
+// const amount = slices % children;
+
+// const random = 4 + 6 + 10 * 10;
+// const random2 = (4 + 6 + 10) * 10;
+
+// console.log(random);
+// console.log(random2);
+
+// console.log(amount);
