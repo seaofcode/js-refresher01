f57d65b580d466fe65cdaad46cf10031e3836753
web-mjp <seaofcode@gmail.com>
2023-06-16 14:53:11 -0500

    added new js file
    JS Object Refresher

---
+++ app02.js
@@ -0,0 +1,7 @@
+// Object Basics
+
+/* Objects are collections of key (property) value pairs.
+Property Values can be strings, numbers, booleans, arrays and
+functions, however if the property value is a function, it is
+called a method.
+*/
