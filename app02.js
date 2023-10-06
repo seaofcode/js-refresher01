653453d6064ebcae63f787432338d441aa90185b
web-mjp <seaofcode@gmail.com>
2023-10-06 11:40:24 -0500

    Update app02.js
    constructing objects, adding and deleting properties from objects

--- app02.js
+++ app02.js
@@ -5,3 +5,29 @@ Property Values can be strings, numbers, booleans, arrays and
 functions, however if the property value is a function, it is
 called a method.
 */
+
+// object literal syntax, {}
+// dot notation
+
+const person = {
+  name: 'john',
+  age: 25,
+  married: true,
+  siblings: ['anna', 'peter'],
+  greet: function (name) {
+    console.log(`Hello, my name is ${name}`);
+  },
+  sayHello(name) {
+    console.log(`Hello, my name is ${name}`);
+  },
+};
+
+console.log(person.age);
+
+const name = person.name;
+console.log(name);
+person.age = 60;
+person.city = 'chicago';
+// delete property
+delete person.siblings;
+console.log(person);
